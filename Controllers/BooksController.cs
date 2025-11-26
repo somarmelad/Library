f35328eed3b4f4afd69df6e9c3ae64553cdcda31
Sofia Stepanova <sonya@mail.ru>
2025-11-26 19:05:41 +0300

    fix BookIndex

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering; // Для SelectList и MultiSelectList
 using Library2.Models;
 using Library2.Data;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library2.Controllers
@@ -24,6 +28,7 @@ namespace Library2.Controllers
                     .ThenInclude(ba => ba.Author)
                 .Include(b => b.BookGenres)
                     .ThenInclude(bg => bg.Genre)
+                .Include(b => b.BookLoans) // Если нужно показывать выдачи
                 .ToListAsync();
             return View(books);
         }
@@ -42,6 +47,7 @@ namespace Library2.Controllers
                     .ThenInclude(ba => ba.Author)
                 .Include(b => b.BookGenres)
                     .ThenInclude(bg => bg.Genre)
+                .Include(b => b.BookLoans)
                 .FirstOrDefaultAsync(m => m.IdBook == id);
 
             if (book == null)
@@ -55,10 +61,14 @@ namespace Library2.Controllers
         // GET: Books/Create
         public IActionResult Create()
         {
-            ViewBag.Publishers = _context.Publishers.ToList();
-            ViewBag.Authors = _context.Authors.ToList();
-            ViewBag.Genres = _context.Genres.ToList();
-            return View();
+            // SelectList для single-select (Publisher)
+            ViewBag.Publishers = new SelectList(_context.Publishers, "Id", "Name"); // Предполагаю поля Id/Name в Publisher
+
+            // MultiSelectList для many-to-many (Authors, Genres)
+            ViewBag.Authors = new MultiSelectList(_context.Authors, "Id", "Name");
+            ViewBag.Genres = new MultiSelectList(_context.Genres, "Id", "Name");
+
+            return View(new Book());
         }
 
         // POST: Books/Create
@@ -68,31 +78,35 @@ namespace Library2.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Добавляем связи с авторами
-                if (selectedAuthors != null)
+                // Добавляем книгу сначала (для генерации IdBook)
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+
+                // Теперь добавляем связи many-to-many
+                if (selectedAuthors != null && selectedAuthors.Any())
                 {
                     foreach (var authorId in selectedAuthors)
                     {
-                        book.BookAuthors.Add(new BookAuthor { BookId = book.IdBook, AuthorId = authorId });
+                        _context.BookAuthors.Add(new BookAuthor { BookId = book.IdBook, AuthorId = authorId });
                     }
                 }
 
-                // Добавляем связи с жанрами
-                if (selectedGenres != null)
+                if (selectedGenres != null && selectedGenres.Any())
                 {
                     foreach (var genreId in selectedGenres)
                     {
-                        book.BookGenres.Add(new BookGenre { BookId = book.IdBook, GenreId = genreId });
+                        _context.BookGenres.Add(new BookGenre { BookId = book.IdBook, GenreId = genreId });
                     }
                 }
 
-                _context.Add(book);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewBag.Publishers = _context.Publishers.ToList();
-            ViewBag.Authors = _context.Authors.ToList();
-            ViewBag.Genres = _context.Genres.ToList();
+
+            // При ошибке валидации: Перезагрузи ViewBag
+            ViewBag.Publishers = new SelectList(_context.Publishers, "Id", "Name", book.PublisherId);
+            ViewBag.Authors = new MultiSelectList(_context.Authors, "Id", "Name");
+            ViewBag.Genres = new MultiSelectList(_context.Genres, "Id", "Name");
             return View(book);
         }
 
@@ -105,6 +119,7 @@ namespace Library2.Controllers
             }
 
             var book = await _context.Books
+                .Include(b => b.Publisher)
                 .Include(b => b.BookAuthors)
                 .Include(b => b.BookGenres)
                 .FirstOrDefaultAsync(m => m.IdBook == id);
@@ -114,9 +129,14 @@ namespace Library2.Controllers
                 return NotFound();
             }
 
-            ViewBag.Publishers = _context.Publishers.ToList();
-            ViewBag.Authors = _context.Authors.ToList();
-            ViewBag.Genres = _context.Genres.ToList();
+            // Выбранные значения для multi-select
+            var selectedAuthorIds = book.BookAuthors.Select(ba => ba.AuthorId).ToList();
+            var selectedGenreIds = book.BookGenres.Select(bg => bg.GenreId).ToList();
+
+            ViewBag.Publishers = new SelectList(_context.Publishers, "Id", "Name", book.PublisherId);
+            ViewBag.Authors = new MultiSelectList(_context.Authors, "Id", "Name", selectedAuthorIds);
+            ViewBag.Genres = new MultiSelectList(_context.Genres, "Id", "Name", selectedGenreIds);
+
             return View(book);
         }
 
@@ -134,11 +154,15 @@ namespace Library2.Controllers
             {
                 try
                 {
-                    // Обновляем связи с авторами
+                    // Удаляем старые связи many-to-many
                     var existingAuthors = _context.BookAuthors.Where(ba => ba.BookId == id);
                     _context.BookAuthors.RemoveRange(existingAuthors);
 
-                    if (selectedAuthors != null)
+                    var existingGenres = _context.BookGenres.Where(bg => bg.BookId == id);
+                    _context.BookGenres.RemoveRange(existingGenres);
+
+                    // Добавляем новые
+                    if (selectedAuthors != null && selectedAuthors.Any())
                     {
                         foreach (var authorId in selectedAuthors)
                         {
@@ -146,11 +170,7 @@ namespace Library2.Controllers
                         }
                     }
 
-                    // Обновляем связи с жанрами
-                    var existingGenres = _context.BookGenres.Where(bg => bg.BookId == id);
-                    _context.BookGenres.RemoveRange(existingGenres);
-
-                    if (selectedGenres != null)
+                    if (selectedGenres != null && selectedGenres.Any())
                     {
                         foreach (var genreId in selectedGenres)
                         {
@@ -158,6 +178,7 @@ namespace Library2.Controllers
                         }
                     }
 
+                    // Обновляем саму книгу
                     _context.Update(book);
                     await _context.SaveChangesAsync();
                 }
@@ -174,9 +195,13 @@ namespace Library2.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewBag.Publishers = _context.Publishers.ToList();
-            ViewBag.Authors = _context.Authors.ToList();
-            ViewBag.Genres = _context.Genres.ToList();
+
+            // При ошибке: Перезагрузи ViewBag с выбранными
+            var selectedAuthorIds = selectedAuthors ?? new List<int>();
+            var selectedGenreIds = selectedGenres ?? new List<int>();
+            ViewBag.Publishers = new SelectList(_context.Publishers, "Id", "Name", book.PublisherId);
+            ViewBag.Authors = new MultiSelectList(_context.Authors, "Id", "Name", selectedAuthorIds);
+            ViewBag.Genres = new MultiSelectList(_context.Genres, "Id", "Name", selectedGenreIds);
             return View(book);
         }
 
@@ -190,6 +215,9 @@ namespace Library2.Controllers
 
             var book = await _context.Books
                 .Include(b => b.Publisher)
+                .Include(b => b.BookAuthors)
+                .Include(b => b.BookGenres)
+                .Include(b => b.BookLoans)
                 .FirstOrDefaultAsync(m => m.IdBook == id);
 
             if (book == null)
@@ -205,13 +233,23 @@ namespace Library2.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            // Удаляем связи many-to-many и BookLoans перед удалением книги
+            var bookAuthors = _context.BookAuthors.Where(ba => ba.BookId == id);
+            _context.BookAuthors.RemoveRange(bookAuthors);
+
+            var bookGenres = _context.BookGenres.Where(bg => bg.BookId == id);
+            _context.BookGenres.RemoveRange(bookGenres);
+
+            var bookLoans = _context.BookLoans.Where(bl => bl.BookId == id); // Предполагаю BookId в BookLoan
+            _context.BookLoans.RemoveRange(bookLoans);
+
             var book = await _context.Books.FindAsync(id);
             if (book != null)
             {
                 _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
